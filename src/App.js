e61c955fc377dd052afefa2e440b7b2a8bd82a66
Yuko Pangestu <yuko.pangestu@paper.id>
2024-12-31 02:07:51 +0700

    adding icon and fixing

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import logo from './logo.svg';
 import './App.css';
+import 'bootstrap-icons/font/bootstrap-icons.css';
 
 function App() {
   return (
@@ -7,12 +8,12 @@ function App() {
         <div class="profile-header">
             <img src="profile-picture.jpg" alt="Profile Picture" class="profile-picture"></img>
             <h1>Yuko Pangestu</h1>
-            <p>Web Developer | Designer</p>
+            <p>Tech Lead | Web Developer | Teamplayer</p>
         </div>
 
         <div class="section">
             <h2>About Me</h2>
-            <p>Passionate web developer with experience in creating responsive and user-friendly websites. Skilled in HTML, CSS, and JavaScript.</p>
+            <p>In need of passionate website programmer? Here I am. I am ready to take any challenge that comes into me.</p>
         </div>
 
         <div class="section">
@@ -27,9 +28,18 @@ function App() {
 
         <div class="section">
             <h2>Contact</h2>
-            <p>Email: <a href="mailto:yuko.pangestu@gmail.com" target='_blank'>yuko.pangestu@gmail.com</a></p>
-            <p>LinkedIn: <a href='https://www.linkedin.com/in/yukopangestu' target='_blank'>yukopangestu</a></p>
-            <p>GitHub: <a href="https://github.com/yukopangestu" target='_blank'>github.com/yukopangestu</a></p>
+            <div style={{display: 'flex', alignItems: 'center'}}>
+                <i class="bi bi-mailbox"></i>
+                <p style={{marginLeft: '10px'}}> <a href="mailto:yuko.pangestu@gmail.com" target='_blank' rel='noreferrer'>yuko.pangestu@gmail.com</a></p>
+            </div>
+            <div style={{display: 'flex', alignItems: 'center'}}>
+                <i class="bi bi-linkedin"></i>
+                <p style={{marginLeft: '10px'}}><a href='https://www.linkedin.com/in/yukopangestu' target='_blank' rel='noreferrer'>yukopangestu</a></p>
+            </div>
+            <div style={{display: 'flex', alignItems: 'center'}}>
+                <i class="bi bi-github"></i>
+                <p style={{marginLeft: '10px'}}><a href="https://github.com/yukopangestu" target='_blank' rel='noreferrer'>github.com/yukopangestu</a></p>
+            </div>
         </div>
     </div>
   );
